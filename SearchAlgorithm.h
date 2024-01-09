62471f61f03cc5d56746f4ec593a2712826687bd
Eduardo Rodriguez-Tello <44586859+ertello@users.noreply.github.com>
2024-01-09 11:52:05 -0600

    Update SearchAlgorithm.h

--- SearchAlgorithm.h
+++ SearchAlgorithm.h
@@ -1,5 +1,5 @@
-#ifndef _ALGORITHM_SEARCH_H_
-#define _ALGORITHM_SEARCH_H_
+#ifndef _SEARCH_ALGORITHM_H_
+#define _SEARCH_ALGORITHM_H_
 
 #include <iostream>
 #include <vector>
@@ -10,9 +10,9 @@ class SearchAlgorithm {
     SearchAlgorithm();
     ~SearchAlgorithm();
     int busquedaSecuencial(std::vector<T> &A, T key, int &compara);
-    //int busquedaSecuencialVectorOrdenado(std::vector<T> &A, int key, int &compara);
-    //int busquedaBinaria(std::vector<T> &A, int key, int &compara);
-    //int busquedaBinariaRecursiva(std::vector<T> &A, int low, int high, int key, int &compara);
+    int busquedaSecuencialVectorOrdenado(std::vector<T> &A, T key, int &compara);
+    int busquedaBinaria(std::vector<T> &A, T key, int &compara);
+    int busquedaBinariaRecursiva(std::vector<T> &A, int low, int high, T key, int &compara);
 
 };
 
@@ -26,6 +26,7 @@ SearchAlgorithm<T>::~SearchAlgorithm() {
 
 }
 
+// O(n)
 template <class T>
 int SearchAlgorithm<T>::busquedaSecuencial(std::vector<T> &A, T key, int &compara) {
   compara = 0;
@@ -40,5 +41,54 @@ int SearchAlgorithm<T>::busquedaSecuencial(std::vector<T> &A, T key, int &compar
     return -1;
 }
 
+// O(n)
+template <class T>
+int SearchAlgorithm<T>::busquedaSecuencialVectorOrdenado(std::vector<T> &A, T key, int &compara) {
+  compara = 0;
+  for (int i = 0; i < A.size(); i++) {
+    compara++;
+    if (key <= A[i]) {
+      compara++;
+      if (key == A[i])
+        return i;
+      else
+        return -1;
+    }
+  }
+  return -1;
+}
 
-#endif  // _ALGORITHM_SEARCH_H_
+// O(log n)
+template <class T>
+int SearchAlgorithm<T>::busquedaBinaria(std::vector<T> &A, T key, int &compara) {
+  int l = 0;
+  int r = A.size() - 1;
+  compara = 0;
+  while (l <= r) {
+    int m = l + (r - l) / 2;
+    compara++;
+    if (key == A[m])
+      return m;
+    else if (key < A[m]) 
+      r = m - 1;
+    else
+      l = m + 1;
+  }
+  return -1;
+}
+
+// O(log n)
+template <class T>
+int SearchAlgorithm<T>::busquedaBinariaRecursiva(std::vector<T> &A, int low, int high, T key, int &compara) {
+  if (low > high)
+    return -1;
+  int m = low + (high - low) / 2;
+  compara++;
+  if (key == A[m]) 
+    return m;
+  else if (key < A[m])
+    return busquedaBinariaRecursiva(A, low, m-1, key, compara);
+  else 
+    return busquedaBinariaRecursiva(A, m+1, high, key, compara);
+}
+#endif  // _SEARCH_ALGORITHM_H_
